be11ee18d99f8c1615464e004b7048accce022af
devLateef <lateefolatayo@gmail.com>
2025-10-22 12:22:27 +0100

    Added resources monitoring method

--- resourcesmonitor.py
+++ resourcesmonitor.py
@@ -3,8 +3,10 @@ import psutil
 def resources_monitor():
     cpu_usage = psutil.cpu_percent(interval=1)
     memory_usage = psutil.virtual_memory().percent
+    disk_usage = psutil.disk_usage('/').percent
     print(f"CPU Usage: {cpu_usage}%")
     print(f"Memory Usage: {memory_usage}%")
+    print(f"Disk Usage: {disk_usage}%")
 
 
 
